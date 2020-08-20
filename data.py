2c46a000c8dfcf62fdcb1dea9f9176938b6f42f0
I-Azy-I <62987235+I-Azy-I@users.noreply.github.com>
2020-08-20 15:29:33 +0200

    Tout
    Fichiers

---
+++ data.py
@@ -0,0 +1,36 @@
+import json
+import random
+import datetime
+class Text_generator():
+    def find_text(self):
+        choice=random.randint(1,self.rand_sum)
+        i=0
+        y=0
+        #print(tags_items)
+        #print(choice)
+        while i<choice:
+            #print(f"-> {tags_items[y][1]+weight_tag}")
+            i=i+(self.tags_items[y][1]*self.weight_text)+self.weight_tag
+            y+=1
+        text=self.data[self.today][self.tags_items[y-1][0]][random.randint(0,len(self.data[self.today][self.tags_items[y-1][0]])-1)]
+        return text
+
+    def get_text(self):
+        if self.today != datetime.datetime.now().strftime("%d.%m"):
+            self.today = datetime.datetime.now().strftime("%d.%m")
+            with open("liste_pays.json") as f:
+                self.data= json.load(f)
+            random.seed()
+
+            self.tags_items=[]
+            self.rand_sum=(self.weight_tag*len(self.data[self.today]))
+            for tag_name, tag_value in self.data[self.today].items():
+                self.tags_items.append((tag_name,len(tag_value)))
+                self.rand_sum=self.rand_sum+(len(tag_value)*self.weight_text)
+
+        return self.find_text()
+
+    def init(self,weight_tag=0, weight_text=1):
+        self.today="0.0"
+        self.weight_tag=weight_tag
+        self.weight_text=weight_text
---
+++ main.py
@@ -0,0 +1,42 @@
+import json
+import data
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout  # one of many layout structures
+from kivy.uix.textinput import TextInput  # allow for ...text input.
+from kivy.uix.button import Button
+kivy.require("1.11.1")
+
+# An actual app is likely to consist of many different
+# "pages" or "screens." Inherit from GridLayout
+class ConnectPage(GridLayout):
+    # runs on initialization
+    def __init__(self, **kwargs):
+        # we want to run __init__ of both ConnectPage AAAAND GridLayout (understanding_super.py)
+        super().__init__(**kwargs)
+
+        self.cols = 1
+        self.text=Label(text='Trinquons!')
+        self.add_widget(self.text)  # used for our grid
+        self.button = Button()
+        self.button.bind(on_press=self.button_get_text) # just take up the spot.
+        self.add_widget(self.button)
+
+    def button_get_text(self, instance):
+        text=test.get_text()
+        self.text.text=f"Trinquon pour la fête nationale de {text[0]}"
+
+        # widgets added in order, so mind the order.
+
+
+
+class EpicApp(App):
+    def build(self):
+        return ConnectPage()
+
+
+if __name__ == "__main__":
+    test= data.Text_generator()
+    test.init()
+    EpicApp().run()
